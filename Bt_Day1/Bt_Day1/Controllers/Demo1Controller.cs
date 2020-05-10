cb631d6ccf5788c50ec95799cbd0203f05d40f8e
truonghiso <truong.nguyennhat1997@gmail.com>
2020-05-10 18:32:05 +0700

    Update Bt_Day1 Second

---
+++ Bt_Day1/Bt_Day1/Controllers/Demo1Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bt_Day1.Controllers
+{
+    public class Demo1Controller : Controller
+    {
+        // Link local:port/Demo1/Chuoi hoac So:
+        public string Chuoi()
+        {
+            return "Hello";
+        }
+
+        public int So()
+        {
+            return new Random().Next(1000, 10000);
+        }
+
+
+        // Hàm code xem Views:
+        public IActionResult demo1_1()
+        {
+            return View();
+        }
+
+        public IActionResult demo1()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bt_Day1/Bt_Day1/Controllers/TuDienDemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bt_Day1.Controllers
+{
+    public class TuDienDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult tudienDemo()
+        {
+            return View();
+        }
+    }
+}
